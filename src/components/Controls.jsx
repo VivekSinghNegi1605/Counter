bfcd0e928b3eacd93804f84b026cc72b16b15a7f
Vivek Singh Negi <viveksinghnegi1605@gmail.com>
2024-05-08 20:57:46 +0530

    Counter

---
+++ src/components/Controls.jsx
@@ -0,0 +1,79 @@
+import { useRef } from "react";
+import { useDispatch } from "react-redux";
+import { counterActions } from "../store/counter";
+import { privacyAction } from "../store/privacy";
+
+const Controls = () => {
+  const dispatch = useDispatch();
+  const inputElement = useRef();
+
+  const handleIncrement = () => {
+    dispatch(counterActions.increment());
+  };
+
+  const handleDecrement = () => {
+    dispatch(counterActions.decrement());
+  };
+
+  const handlePrivacyToggle = () => {
+    dispatch(privacyAction.toggle());
+  };
+
+  const handleAdd = () => {
+    dispatch(counterActions.add(inputElement.current.value));
+    inputElement.current.value = "";
+  };
+
+  const handleSubtract = () => {
+    dispatch(counterActions.subtract(inputElement.current.value));
+    inputElement.current.value = "";
+  };
+
+  return (
+    <>
+      <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={handleIncrement}
+        >
+          +1
+        </button>
+        <button
+          type="button"
+          className="btn btn-success"
+          onClick={handleDecrement}
+        >
+          -1
+        </button>
+        <button
+          type="button"
+          className="btn btn-success"
+          onClick={handlePrivacyToggle}
+        >
+          Privacy
+        </button>
+      </div>
+      <div className="d-grid gap-2 d-sm-flex justify-content-sm-center m-2">
+        <input
+          type="text"
+          placeholder="Enter number"
+          ref={inputElement}
+          style={{ width: "110px" }}
+        />
+        <button type="button" className="btn btn-info" onClick={handleAdd}>
+          Add
+        </button>
+        <button
+          type="button"
+          className="btn btn-danger"
+          onClick={handleSubtract}
+        >
+          Subtract
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Controls;
